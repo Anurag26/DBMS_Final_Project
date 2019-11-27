227f7543f91481ef54929071a6a88ae5caef8da3
unknown <sajagporwal@gmail.com>
2019-11-27 01:57:00 -0500

    login set

---
+++ client/src/components/Navbar/navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {Navbar} from 'react-bootstrap';
+import {Nav} from 'react-bootstrap';
+import {Form} from 'react-bootstrap';
+import {FormControl} from 'react-bootstrap';
+import {Button,ResponsiveEmbed} from 'react-bootstrap';
+
+class NavbarClass extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+    </Form>
+  </Navbar>
+            </div>
+        );
+    }
+}
+
+export default NavbarClass;
