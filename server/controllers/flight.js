6208665315f07e9d0c875f067dc46004a82c9c1b
Rahul Pandey <pandey.ra@husky.neu.edu>
2019-12-10 00:47:11 -0500

    added successfull and warning messages

--- server/controllers/flight.js
+++ server/controllers/flight.js
@@ -1,19 +1,51 @@
 var Flight = require('../models/flight');
+const elasticsearch = require('elasticsearch');
 
 //Simple version, without validation or sanitation
 exports.test = function (req, res) {
     res.send('Greetings from the flight test controller!');
 };
 
+const elasticClient = new elasticsearch.Client({
+      host: process.env.ELASTICSEARCH_URI,
+      log: 'trace'
+ });
+
 exports.flight_create = function (req, res, next) {
+
+    let dataToSubmit = {
+        "airline": req.body.airline,
+        "call_sign": req.body.call_sign,
+        "make_name": req.body.make_name,
+        "origin_code": req.body.origin_code,
+        "origin_name": req.body.origin_name,
+        "origin_location": req.body.origin_location,
+        "destination_code": req.body.destination_code,
+        "destination_name": req.body.destination_name,
+        "destination_location": req.body.destination_location,
+        "dateTakeOff": req.body.dateTakeOff,
+        "price": req.body.price,
+        "capacity": req.body.capacity,
+        "manager": req.body.manager,
+        "managerId": req.body.managerId
+    }
+
     var flight = new Flight(
         {
+            airline: req.body.airline,
             call_sign: req.body.call_sign,
-            make: req.body.make,
-            destination: req.body.destination,
-            origin: req.body.origin,
-            duration: req.body.duration,
-            capacity: req.body.capacity
+            make_name: req.body.make_name,
+            origin_code: req.body.origin_code,
+            origin_name: req.body.origin_name,
+            origin_location: req.body.origin_location,
+            destination_code: req.body.destination_code,
+            destination_name: req.body.destination_name,
+            destination_location: req.body.destination_location,
+            dateTakeOff: req.body.dateTakeOff,
+            price: req.body.price,
+            capacity: req.body.capacity,
+            manager: req.body.manager,
+            managerId: req.body.managerId
         }
     );
 
@@ -21,6 +53,28 @@ exports.flight_create = function (req, res, next) {
         if (err) {
             return next(err);
         }
+
+        //Adding index to elastic cluster
+        var payload = dataToSubmit;
+        var indexName = "flights_bookings";
+        var docType = "flights";
+        var id = (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+
+        elasticClient.index({
+                                index: indexName,
+                                type: docType,
+                                id: id,
+                                body: payload
+                            }).then(function (resp) {
+            res.status(200);
+            console.log("flight indexed successfully");
+            return res.json(resp)
+        }, function (err) {
+            res.status(500)
+            return res.json(err)
+        });
+
+        //Sending result in response
         res.send(flight);
     })
 };
--- server/controllers/hotel.js
+++ server/controllers/hotel.js
@@ -1,20 +1,40 @@
 const Hotel = require('../models/hotel');
+const elasticsearch = require('elasticsearch');
 
 //Simple version, without validation or sanitation
 exports.test = function (req, res) {
     res.send('Greetings from the hotel test controller!');
 };
 
+const elasticClient = new elasticsearch.Client({
+                                                   host: process.env.ELASTICSEARCH_URI,
+                                                   log: 'trace'
+   });
+
 exports.hotel_create = function (req, res, next) {
+
+    let dataToSubmit = {
+        "name":req.body.name,
+        "total_rooms":req.body.total_rooms,
+        "totalCapacity":req.body.totalCapacity,
+        "country":req.body.country,
+        "phone":req.body.phone,
+        "room_type":req.body.room_type,
+        "price":req.body.price,
+        "address_street":req.body.address_street,
+        "address_city":req.body.address_city,
+        "managerId": req.body.managerId
+    }
+
     var hotel = new Hotel(
         {
             name:req.body.name,
             total_rooms:req.body.total_rooms,
             totalCapacity:req.body.totalCapacity,
             country:req.body.country,
+            manager: req.body.manager,
             phone:req.body.phone,
             room_type:req.body.room_type,
-            manager:req.body.manager,
             price:req.body.price,
             address_street:req.body.address_street,
             address_city:req.body.address_city,
@@ -26,6 +46,27 @@ exports.hotel_create = function (req, res, next) {
         if (err) {
             return next(err);
         }
+
+        //Adding index to elastic cluster
+        var payload = dataToSubmit;
+        var indexName = "hotels_bookings";
+        var docType = "hotels";
+        var id = (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+
+        elasticClient.index({
+                                index: indexName,
+                                type: docType,
+                                id: id,
+                                body: payload
+                            }).then(function (resp) {
+            res.status(200);
+            console.log("Hotel indexed successfully");
+            return res.json(resp)
+        }, function (err) {
+            res.status(500)
+            return res.json(err)
+        });
+
         res.send(hotel);
     })
 };
